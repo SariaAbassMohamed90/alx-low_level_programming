832cae0e8d40eb5e0f628d51491cbf3254159e90
SariaAbassMohamed90 <saryaabass90@gmail.com>
2023-09-13 13:48:03 -0700

    Update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,13 @@
 
 /**
  * _islower -Checks for lowercase character
- * $c: The to be checked
+ * @c: The to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
-
 {
-
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
