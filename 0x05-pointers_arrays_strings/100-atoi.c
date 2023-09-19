99b717cde2c24916e1d75b6c366a95c88a7774de
SariaAbassMohamed90 <saryaabass90@gmail.com>
2023-09-19 12:52:25 -0700

    Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,36 +7,17 @@
  */
 int _atoi(char *s)
 {
-	int i, d, nb, len, f, digit;
+	unsigned int num = 0;
+	int sigh = 1;
 
-	i = 0;
-	d = 0;
-	nb = 1;
-	len = 0;
-	f = 0;
-	digit = 0;
+	do {
+		if (*s == '-')
+			sigh *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			nb = nb * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (nb);
+	return (num * sigh);
 }
