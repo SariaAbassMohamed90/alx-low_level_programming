2c9bbbf824193b343b0d5b10c4d597f8c88f11fa
SariaAbassMohamed90 <saryaabass90@gmail.com>
2023-09-25 14:56:42 -0700

    Updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - Entry point
- * @s: Pointed destination
- * @b: Constant byte
- * @n: Bytes
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
  * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
