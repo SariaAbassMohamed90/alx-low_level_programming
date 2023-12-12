cc2db75e51299851db7674c67ef76ddec571de6a
SariaAbassMohamed90 <saryaabass90@gmail.com>
2023-12-11 17:00:15 -0800

    Updated

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - isdigit
+ * @s: integer
+ * Return: prints alphabet
+ */
+int _strlen(char *s)
+{
+	int i;
+	int len;
+
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+	return (len);
+}
+
---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * _islower - islower
+ * @c: integer
+ * Return: prints alphabet
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
